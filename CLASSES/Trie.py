f4e496716fff5a105443500253d8bf6bb8eb1de6
Jathin Korrapati <jkorr45@gmail.com>
2023-05-18 02:05:24 -0700

    stuff

---
+++ CLASSES/Trie.py
@@ -0,0 +1,47 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        r = self.root
+        for c in word:
+            if c not in r.children:
+                r.children[c] = TrieNode()
+            r = r.children[c]
+        r.end = True
+
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        r = self.root
+        for c in word:
+            if c not in r.children:
+                return False
+            r = r.children[c]
+        return r.end
+        
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        r = self.root
+        for c in prefix:
+            if c not in r.children:
+                return False
+            r = r.children[c]
+        return True
