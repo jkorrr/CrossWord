533a3b8e7f26617bf29d264c390a90d0f3371f96
Jathin Korrapati <jkorr45@gmail.com>
2023-05-19 01:22:48 -0700

    sdf

--- BACKEND/crossword.py
+++ BACKEND/crossword.py
@@ -2,16 +2,20 @@ from pre_process import process_data
 import random
 from setup import make_grid, generate_pattern
 from CLASSES.Graph import Graph
-from CLASSES.Trie import Trie
+from CLASSES.Trie import TrieNode
 
-n = random.randint(0, 10)
+n = random.randint(5, 10)
 
 data, diff = process_data()
 grid = make_grid(n)
 grid = generate_pattern(grid)
 
-root = Trie()
-for word in data.keys():
-    root.insert(word)
+root = TrieNode()
+words = list(data.keys())
 
-print(root)
+
+for word in words:
+    if type(word) is str:
+        root.insert(word)
+
+print(root.print_trie())
