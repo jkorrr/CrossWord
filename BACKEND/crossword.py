1be502dcffedf0b901051cfd6db5201672328403
Jathin Korrapati <jkorr45@gmail.com>
2023-05-19 01:38:08 -0700

    sd

--- BACKEND/crossword.py
+++ BACKEND/crossword.py
@@ -2,7 +2,7 @@ from pre_process import process_data
 import random
 from setup import make_grid, generate_pattern
 from CLASSES.Graph import Graph
-from CLASSES.Trie import TrieNode
+from CLASSES.Trie import Trie
 
 n = random.randint(5, 10)
 
@@ -10,7 +10,7 @@ data, diff = process_data()
 grid = make_grid(n)
 grid = generate_pattern(grid)
 
-root = TrieNode()
+root = Trie()
 words = list(data.keys())
 
 
